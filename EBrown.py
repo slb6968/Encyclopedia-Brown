f9b443b4098b97995d735b0cefa672aebead2de6
Shavon Brown <slb6968@hotmail.com>
2015-12-15 00:22:17 -0500

    Create EBrown.py

---
+++ EBrown.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Encyclopedia Brown, a traveler's resource"""
+
+import random
+import reviews
+import travelhangman
+
+def printEBmenu():
+    """prints the main menu."""
+    EBmenu = ['Add a new review',
+              'Search and read reviews',
+              'Search locale information',
+              'Play EB\'s Travel Hangman',
+              'Feeling Lucky?',
+              'Exit?']
+    i = 1
+    print '-' * 55
+    for item in EBmenu:
+        print i, " - ", item
+        i += 1
+    print '-' * 55, '\n'
+
+
+print ' ' * 18, 'WELCOME TO ENCYCLOPEDIA BROWN'
+print 'a resource for world travelers to learn, explore, and communicate'
+print '_' * 65, '\n'
+
+exit = False
+while not exit:
+    printEBmenu()
+    choice = raw_input('What would you like to do? ')
+    if choice == '1':
+        reviews.add_review()
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        travelhangman.go_play_hm()
+    elif choice == '5':
+        print '\nENCYCLOPEDIA BROWN\'s magic 8 ball says you should travel to:'
+        print '\n{0} in {1}\n'.format(random.choice(reviews.locales).upper(),
+                          random.randint(2016, 2046))
+    elif choice == '6':
+        exit = True
+        print 'Thanks for checking out Encyclopedia Brown!'
